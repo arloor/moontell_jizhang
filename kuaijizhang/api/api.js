8c388f9cc6c7cec69c55ea2007c09e86911f8bf8
arloor <admin@arloor.com>
2018-07-18 00:08:57 +0800

    Initial commit

---
+++ kuaijizhang/api/api.js
@@ -0,0 +1,66 @@
+const prefix = "https://nywc.moontell.cn/nywc/api/";
+// const prefix = "https://localhost/api/";//测试用
+
+// const api = require("../../../api/api.js");
+// api.checkIsCollected({
+//   data: {
+//     dkey: options.dkey,
+//     openId: openId,
+//   },
+//   success: (res) => {
+//     this.setData({
+//       isCollected: res.data,
+//     })
+//   }
+// });
+
+const api = require("/api/api.js");
+
+const wxRequest = (params, url) => {
+  wx.showLoading({
+    title: 'Loading...',
+  });
+  wx.request({
+    url: url,
+    data: params.data || '',
+    header: {
+      'content-type': 'application/json'
+    },
+    method: params.method || 'GET',
+    success: (res) => {
+      params.success && params.success(res);
+      wx.hideLoading();
+    },
+    fail: (res) => {
+      params.fail && params.fail(res);
+      wx.hideLoading();
+    },
+    complete: (res) => {
+      params.complete && params.complete(res);
+      wx.hideLoading();
+    }
+  })
+} 
+
+// home
+const getCardsList = (params) => wxRequest(params, prefix + 'card/details');
+const getCardByDkey = (params) => wxRequest(params, prefix + 'card/singleCardDetails');
+const submitComment = (params) => wxRequest(params, prefix + 'design/newComment');
+const collectIWant = (params) => wxRequest(params, prefix + 'card/iwant');
+const checkIsCollected = (params) => wxRequest(params, prefix + 'member/wantIt');
+const getIWant = (params) => wxRequest(params, prefix + 'member/getiwant');
+const uploadImages = (params) => wxRequest(params, prefix + 'design/insert');
+const iDontWant = (params) => wxRequest(params, prefix + 'card/iDontWant');
+const cateSearch = (params) => wxRequest(params, prefix + 'card/getByTheme');
+
+module.exports = {
+  getCardsList,
+  getCardByDkey,
+  submitComment,
+  collectIWant,
+  checkIsCollected,
+  getIWant,
+  uploadImages,
+  iDontWant,
+  cateSearch
+}
