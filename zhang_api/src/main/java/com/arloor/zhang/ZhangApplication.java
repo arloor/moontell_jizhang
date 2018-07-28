83244c5f4a3e1d4f8550be0984ee4927b75bb0c8
arloor <admin@arloor.com>
2018-07-29 00:47:40 +0800

    增加后端api模块并配置依赖

---
+++ zhang_api/src/main/java/com/arloor/zhang/ZhangApplication.java
@@ -0,0 +1,12 @@
+package com.arloor.zhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class ZhangApplication {
+    public static void main(String[] args){
+        ApplicationContext app=SpringApplication.run(ZhangApplication.class);
+    }
+}
